6fcb240b3ce959de66a840f967d65ddef3d7d183
Gaurav Singh <Gauravsingh.code@gmail.com>
2025-08-27 08:32:34 +0530

    Update settings.py

--- PasswordManager/settings.py
+++ PasswordManager/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
